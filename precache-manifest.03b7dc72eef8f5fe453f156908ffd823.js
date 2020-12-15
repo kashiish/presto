0cc3b4bfc41731bde08c4af55974328c507ba6a0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-15 16:26:12 +0000

    Deploy kashiish/presto to github.com/kashiish/presto.git:gh-pages

---
+++ precache-manifest.03b7dc72eef8f5fe453f156908ffd823.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896643926ef1243e69025df0fc3caa0e",
+    "url": "/presto/index.html"
+  },
+  {
+    "revision": "fb0407b346930d4f8924",
+    "url": "/presto/static/css/main.b9b24ed5.chunk.css"
+  },
+  {
+    "revision": "e3ce77c737210e5cc846",
+    "url": "/presto/static/js/2.8b3db3f8.chunk.js"
+  },
+  {
+    "revision": "fb0407b346930d4f8924",
+    "url": "/presto/static/js/main.0edb6606.chunk.js"
+  },
+  {
+    "revision": "a73a480302da5b486363",
+    "url": "/presto/static/js/runtime~main.c2e01515.js"
+  },
+  {
+    "revision": "31ffd1dcd3e60f4e1d37d91855e74a3e",
+    "url": "/presto/static/media/vegetarian-mark.31ffd1dc.svg"
+  }
+]);
